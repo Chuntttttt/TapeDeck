a09785b4e55126fa0498a5bf47523c7109c97643
James Power <jep@lepoer.us>
2025-10-14 11:22:13 -0400

    Fix WriteString to handle both return values.

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -30,7 +30,7 @@ func logDebug(format string, v ...interface{}) {
 	msg := fmt.Sprintf(format, v...)
 	log.Print(msg)
 	if debugLog != nil {
-		_ = debugLog.WriteString(time.Now().Format("2006/01/02 15:04:05") + " " + msg + "\n")
+		_, _ = debugLog.WriteString(time.Now().Format("2006/01/02 15:04:05") + " " + msg + "\n")
 		_ = debugLog.Sync()
 	}
 }
